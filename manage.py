4a2c8cb235d27ffd540e51d706703a6cb4068bb2
quang <nlq@dynadna.com>
2020-02-01 11:52:28 -0500

    import management from django.core

--- manage.py
+++ manage.py
@@ -2,8 +2,8 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-from django,core import management
-sys.path.append("..")
+from django.core import management
+
 
 
 def main():
