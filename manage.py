783da8eaee1b0605fa0474152e0c9edd281cd57c
quang <nlq@dynadna.com>
2020-02-02 09:25:59 -0500

    fix import

--- manage.py
+++ manage.py
@@ -2,14 +2,14 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+from django import core
 
 
 
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myblognlq.settings')
     try:
-        from django.core.management import execute_from_command_line
+        from core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
